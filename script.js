9579be369c660e0673b4ba51020e542631068b06
Sujal Thapa <suzaalproff@gmail.com>
2024-01-31 19:39:01 -0500

    Design the card This is my first javascript assignment so I am going step by step over all html is over and css and javascript is left todo

---
+++ script.js
@@ -0,0 +1,7 @@
+let detailsForm = document.getElementById('#food_detail_form');
+detailsForm.addEventListener('submit' handlesubmitform);
+function handlesubmitform(event){
+event.preventDefault;
+
+};
+
